d9c507f963df9df36ebef9ce56f96b16bf0db9d7
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-16 20:24:57 +0000

    Using CSS modules with React

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,5 +1,7 @@
+import css from "./css/Sidebar.module.css"
+
 function Sidebar() {
-    return <div>
+    return <div className = {css.sidebar} >
                 <a>MyPhotos</a>
                 <a>My Illustrations</a>
                 <a>MyPaintings</a>
