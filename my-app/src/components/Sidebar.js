558006aea43dae874671c512cccfc06836f4c997
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-16 20:19:42 +0000

    Using Functional Components Challenge

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,9 @@
+function Sidebar() {
+    return <div>
+                <a>MyPhotos</a>
+                <a>My Illustrations</a>
+                <a>MyPaintings</a>
+            </div>
+}
+
+export default Sidebar
