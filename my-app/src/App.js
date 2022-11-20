798443590f49a10778f4b14ff7490894c8b802d7
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-20 13:07:47 +0000

    Rendering lists challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import css from './App.module.css';
 import NavBarForm from './components/NavBarForm';
 import Sidebar from "./components/Sidebar"
+import Content from './components/Content';
 
 function App() {
   return (
     <div className={css.App}>
       <Sidebar />
       <NavBarForm />
+      <Content />
 
     </div>
   );
